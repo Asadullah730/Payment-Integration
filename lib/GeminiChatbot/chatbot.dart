03b04c85543e31de982c834a73b3990839814d31
Asadullah730 <asadkhan69234@gmail.com>
2025-08-28 14:02:44 +0500

    improving the UI of chat

--- lib/GeminiChatbot/chatbot.dart
+++ lib/GeminiChatbot/chatbot.dart
@@ -1,11 +1,10 @@
 import 'dart:io';
-import 'dart:math';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:intl/intl.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class ChatbotScreen extends StatefulWidget {
@@ -31,8 +30,8 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
 
   ChatUser geminiUser = ChatUser(
     id: '1',
-    firstName: 'Gemini',
-    profileImage: 'assets/images/bard.png',
+    firstName: ' Tinni Response',
+    profileImage: 'assets/images/asad.jpg',
   );
 
   @override
@@ -43,55 +42,130 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Gemini Chat',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0XFF222222),
+        appBar: AppBar(
+          title: const Text(
+            'Tinni Chatbot',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
+          centerTitle: true,
+          backgroundColor: Color(0XFF2C2C2C),
         ),
-        centerTitle: true,
-        backgroundColor: Colors.green,
+        body: _buildUI(),
       ),
-      body: _buildUI(),
     );
   }
 
   Widget _buildUI() {
-    return Stack(
-      children: [
-        DashChat(
-          inputOptions: InputOptions(
-            alwaysShowSend: true,
-            leading: [
-              IconButton(icon: const Icon(Icons.image), onPressed: _sendMedia),
-            ],
-            autocorrect: true,
-            cursorStyle: CursorStyle(color: Colors.blue),
-          ),
-          currentUser: currentUser,
-          onSend: _onSendMessage,
-          messages: messages,
-        ),
-        Positioned(
-          bottom: 7,
-          right: 45,
-          child: IconButton(
-            icon: Icon(
-              _isListening ? Icons.mic : Icons.mic_none,
-              color: _isListening ? Colors.green : Colors.red,
+    final size = MediaQuery.of(context).size;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Column(
+          children: [
+            Expanded(
+              child: DashChat(
+                messageListOptions: MessageListOptions(
+                  dateSeparatorBuilder: (date) {
+                    String formattedDate = DateFormat(
+                      'MMMM dd, yyyy',
+                    ).format(date);
+
+                    return Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: constraints.maxHeight * 0.01,
+                      ),
+                      child: Center(
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(
+                            maxWidth: constraints.maxWidth * 0.7,
+                          ),
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                              vertical: constraints.maxHeight * 0.004,
+                              horizontal: constraints.maxWidth * 0.03,
+                            ),
+                            decoration: BoxDecoration(
+                              color: const Color(0XFF2D2D2D),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Text(
+                              formattedDate,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: constraints.maxWidth * 0.035,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white54,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                inputOptions: InputOptions(
+                  inputTextStyle: const TextStyle(color: Colors.white),
+                  cursorStyle: const CursorStyle(color: Colors.greenAccent),
+                  inputDecoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xFF121B22),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: size.height * 0.012,
+                      horizontal: size.width * 0.04,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      borderSide: BorderSide.none,
+                    ),
+                    hintText: "Say Something to Tinni...",
+                    hintStyle: TextStyle(
+                      color: Colors.white70,
+                      fontSize: size.width * 0.04,
+                    ),
+                    prefixIcon: IconButton(
+                      icon: const Icon(Icons.camera),
+                      onPressed: _sendMedia,
+                      iconSize: size.width * 0.07,
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isListening ? Icons.mic : Icons.mic_none,
+                        color: _isListening ? Colors.green : Colors.white54,
+                        size: size.width * 0.075,
+                      ),
+                      onPressed: _listen,
+                    ),
+                  ),
+                  autocorrect: true,
+                ),
+                currentUser: currentUser,
+                onSend: _onSendMessage,
+                messages: messages,
+                messageOptions: const MessageOptions(
+                  showTime: true,
+                  currentUserTimeTextColor: Colors.white,
+                  currentUserContainerColor: Color(0XFF005C4B),
+                  currentUserTextColor: Colors.white,
+                  containerColor: Color(0XFF353535),
+
+                  textColor: Colors.white,
+                  // showCurrentUserAvatar: true,
+                ),
+              ),
             ),
-            onPressed: _listen,
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     );
   }
 
-  /// Speech Recognition Start/Stop
   void _listen() async {
     if (!_isListening) {
       bool available = await _speech.initialize(
@@ -104,7 +178,9 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
           }
         },
         onError: (error) {
-          if (kDebugMode) print("ERROR: $error");
+          if (kDebugMode) {
+            print("ERROR: $error");
+          }
           setState(() {
             _isListening = false;
           });
@@ -131,7 +207,9 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
           },
         );
       } else {
-        if (kDebugMode) print("Speech recognition not available");
+        if (kDebugMode) {
+          print("Speech recognition not available");
+        }
       }
     } else {
       _speech.stop();
@@ -165,7 +243,7 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
           },
           onDone: () {
             String fullResponse = responseBuffer.toString().trim();
-            if (kDebugMode) print("Gemini full response: $fullResponse");
+            if (kDebugMode) print("Tinni full response: $fullResponse");
 
             ChatMessage newMessage = ChatMessage(
               user: geminiUser,
@@ -178,8 +256,9 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
             });
           },
           onError: (e) {
-            if (kDebugMode)
+            if (kDebugMode) {
               print("ERROR WHILE USER SEND MESSAGE : ${e.toString()}");
+            }
           },
         );
   }
@@ -197,9 +276,13 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
         ],
       );
       _onSendMessage(mediaMessage);
-      if (kDebugMode) print("Selected image path: ${image.path}");
+      if (kDebugMode) {
+        print("Selected image path: ${image.path}");
+      }
     } else {
-      if (kDebugMode) print("No image selected");
+      if (kDebugMode) {
+        print("No image selected");
+      }
     }
   }
 }
